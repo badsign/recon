0fa89bfef6de16d9613a6298ad2c6e1a4af266ae
badsign <badsign@gmail.com>
2018-03-22 00:14:11 -0400

    Update certspotter.py
    Refactor, add iterative list building, temporary diagnostic output

--- certspotter.py
+++ certspotter.py
@@ -1,3 +1,7 @@
+# This command will grab json and display an array of all domain entries at one place in the cert array object (0 in this case).
+# Further iteration of the objects is needed to get all the dns_names from every cert entry
+# curl -s 'https://certspotter.com/api/v0/certs?domain=rockstargames.com' |     python -c "import sys, json; obj=json.load(sys.stdin); print '\n'.join(obj[0]['dns_names'])"
+
 #!/usr/bin/python
 import sys, json, argparse
 from subprocess import Popen, PIPE
@@ -7,37 +11,65 @@ parser = argparse.ArgumentParser(description='Domain recon tool that leverages t
 parser.add_argument('domain', help="Extract domains for the given domain and all sub-domains")
 args = parser.parse_args()
 
-# fetch cert(s) for domain. returns json
-obj = Popen(['curl', '-s', 'https://certspotter.com/api/v0/certs?domain=' + args.domain], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-output, err = obj.communicate("")
-rc = obj.returncode
 
-# decode json
-loaded = json.loads(output)
+def getcertificates(domain):
+    # fetch cert(s) for domain. returns json
+    obj = Popen(['curl', '-s', 'https://certspotter.com/api/v0/certs?domain=' + domain], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = obj.communicate("")
+    rc = obj.returncode
+    
+    # decode and return json
+    return json.loads(output)
 
-dns_names = []
-domainlist = []
-i = 0
+def getdomainlist(certs):
+    dns_names = []
+    domainlist = []
+    i = 0
 
-while i < len(loaded):
-    # iterate certs and extract the dns_names node from each
-    dns_names.append(loaded[i]['dns_names'])
+    while i < len(certs):
+        # iterate certs and extract the dns_names node from each
+        dns_names.append(certs[i]['dns_names'])
 
-    # each dns_names node contains a list of domains
-    for domains in dns_names:
-        # extract each domain from the list
-        for domain in domains:
-            domainlist.append(domain)
+        # each dns_names node contains a list of domains
+        for domains in dns_names:
+            # extract each domain from the list
+            for domain in domains:
+                domainlist.append(domain)
 
-    i += 1
+        i += 1
 
-# converting to a set eliminates duplicates
-domainset = set(domainlist)
+    return domainlist
 
-# converting back to a list will allow sorting
-domainlist = list(domainset)
-domainlist.sort()
+def reduceandsortdomainlist(domainlist):
+    # converting to a set eliminates duplicates
+    domainset = set(domainlist)
 
-# print output, save to a file, etc.
-for domain in domainlist:
+    # converting back to a list will allow sorting
+    domainlist = list(domainset)
+    domainlist.sort()
+    return domainlist
+
+def getrecursivedomainlist(domainlist):
+    mergedlist = []
+    for domain in domainlist:
+        mergedlist += run(domain)
+    return mergedlist
+
+def run(domain):
+    certs = getcertificates(domain)
+    return reduceandsortdomainlist(getdomainlist(certs))
+
+domainlist = run(args.domain)
+recursivedomainlist = reduceandsortdomainlist(getrecursivedomainlist(domainlist))
+
+for domain in recursivedomainlist:
     print domain
+
+print '\n::DIAGNOSTICS::'
+print 'Non-recursive domain list length\t\t{}'.format(len(domainlist))
+print 'Recursive domain list length\t\t\t{}'.format(len(recursivedomainlist))
+print 'Additional domains found by being recursive\t{}\n'.format(len(recursivedomainlist) - len(domainlist))
+
+## TODO: Make iterative list building optional.  
+## It adds a lot of overhead and in some cases it doesn't
+## provide any benefit such as with small businesses
